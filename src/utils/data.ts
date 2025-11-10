f9829903211c1ecb1f33af2f742d0cff127a4382
MehdiZnayzen <mehdiznayen@gmail.com>
2025-11-10 13:12:28 +0100

    Supprimer le fichier index.html

--- src/utils/data.ts
+++ src/utils/data.ts
@@ -7,7 +7,7 @@ import { FullMenuType } from '@/app/types/fullmenu'
 
 export const HeaderData: HeaderItem[] = [
   { label: 'Réserver', href: 'https://wa.me/212664784101'},
-  { label: 'Menu', href: '/index.html' },
+  { label: 'Menu', href: '/5-MENU 151125 site.pdf' },
   { label: 'Restaurants', href: '#restaurants' },
 ]
 
@@ -150,7 +150,7 @@ export const FooterLinkData: FooterLinkType[] = [
     section: 'Cucina Napoli',
     links: [
       { label: 'Réserver', href: 'https://wa.me/212664784101' },
-      { label: 'Menu', href: '/index.html' },
+      { label: 'Menu', href: '/5-MENU 151125 site.pdf' },
       { label: 'Restaurants', href: '#restaurants' },
     ],
   },
